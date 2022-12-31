0c60d72760745f5ab7e2ed80417033b3c3d05bb3
winkey83 <winkey83@gmail.com>
2022-12-31 13:15:30 +0900

    frontend setup

--- src/server.js
+++ src/server.js
@@ -1,7 +1,12 @@
-import express from "express"
+import express, { application } from "express"
 
 const app = express();
 
-console.log("hello");
+app.set("view engine", "pug");
+app.set("views", __dirname+"/views");
+app.use("/public", express.static(__dirname+"/public"));
+app.get("/", (req, res) => res.render("home"));
 
-app.listen(3000);
+const handleListen = () => { console.log('listening...') };
+
+app.listen(3000, handleListen);
